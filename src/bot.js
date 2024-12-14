7f3778e6ea334fb40c724f58a2bd3faa87cbad80
debojyoti10CC <debojyotidm4@gmail.com>
2024-12-14 21:43:15 +0530

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,12 @@
+<script>
+    window.embeddedChatbotConfig = {
+    chatbotId: "010bm1cmIXwvDP1gFdH-i",
+    domain: "www.chatbase.co"
+    }
+    </script>
+    <script
+    src="https://www.chatbase.co/embed.min.js"
+    chatbotId="010bm1cmIXwvDP1gFdH-i"
+    domain="www.chatbase.co"
+    defer>
+    </script>
